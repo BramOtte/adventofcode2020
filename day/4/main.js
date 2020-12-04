65c1c97c2afd733f08d78a04694708967cd03f53
Bram Otte <59796107+BramOtte@users.noreply.github.com>
2020-12-04 11:48:01 +0100

    attempt at day 4

--- day/4/main.js
+++ day/4/main.js
@@ -1,19 +1,67 @@
 setDayNumber(4);
-testDay("--- Day 4 ---", getInput, part1, part2);
+testDay("--- Day 4: Passport Processing ---", getInput, part1, part2);
 
 async function getInput(href){
     return (await fetch(href).then(res=>res.text()))
-        .split("\n").slice(0, -1)
+        .split(/(\n[\s^\n]*){2}/g)
+        .map(row=>row
+            .split(/\s+/g)
+            .map(cell=>cell.split(":"))
+            .reduce((obj, kv)=>{
+                const [key, value] = kv;
+                obj[key] = value;
+                return obj;
+            }, {})
+        )
     ;
 }
+// cid is ignored
+const requiredFields = "byr iyr eyr hgt hcl ecl pid".split(" ");
 function part1(input){
-    let result = 1;
-    return result;
+    const correct = input.filter(obj=>
+            !requiredFields.some((field)=>!obj[field])
+        )
+    return correct.length;
 }
-
+function within(value, min, max){
+    return value >= min && value <= max;
+}
+const eyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+const fieldRequirements = {
+    byr:(string)=>within(parseInt(string), 1920, 2002),
+    iyr:(string)=>within(parseInt(string), 2010, 2020),
+    eyr:(string)=>within(parseInt(string), 2020, 2030),
+    hgt:(string)=>
+        (
+            (/\d{3}cm/).test(string) && 
+            within(parseInt(string), 150, 193) 
+        ) ||
+        (
+            (/\d{2}in/).test(string) &&
+            within(parseInt(string), 59, 76)
+        ),
+    hcl:(string)=>(/#[0-9a-f]{6}/).test(string),
+    ecl:(string)=>eyeColors.includes(string),
+    pid:(string)=>(/\d{9}/).test(string)
+}
 function part2(input){
-    let result = 2;
-    return result;
+    test2(input);
+    const correct = input.filter(obj=>
+            ! Object.keys(fieldRequirements)
+            .some(key=>!(
+                obj[key] && fieldRequirements[key](obj[key])
+            ))
+        )
+    console.log(correct.map(obj=>obj.pid));
+    return correct.length;
 }
 
+function test2(input){
+    for (const key in fieldRequirements){
+        const correct = input.filter(
+            obj=>(obj[key] && fieldRequirements[key](obj[key]) )
+        )
+        console.log(key, correct.map(obj=>obj[key]));
+    }
+}
 
