874c366da6cd41bf9bc56ef1d4bb1c34b7d771e1
Bram Otte <59796107+BramOtte@users.noreply.github.com>
2020-12-08 14:06:22 +0100

    add suport for comments :)

--- day/8/main.js
+++ day/8/main.js
@@ -7,8 +7,9 @@ async function getInput(href){
         .split(/\r*\n/)
         .filter( (line) => (/^\w{3} [+-]\d+/).test(line) )
         .map( (line) => {
-            const [opp, val] = line.split(" ");
-            return Object.freeze({opp:opp, val:parseInt(val)});
+            const [code, comment] = line.split("//");
+            const [opp, val] = code.split(" ");
+            return Object.freeze({opp:opp, val:parseInt(val), comment});
         })
     );
 }
@@ -16,7 +17,6 @@ class Program {
     pc = 0;
     acc = 0;
     trace = [];
-    static maxTime = 1_000_000;
     reset(){
         this.pc = 0;
         this.acc= 0;
@@ -29,16 +29,13 @@ class Program {
             const code = lines[this.pc];
             if (pcs.has(this.pc)){
                 let lastI = pcs.get(this.pc);
-                this.trace.push(this.getNowTrace());
                 const cTrace = this.trace.slice(lastI);
                 const objectTrace = Program.objectTrace(lines, cTrace);
-                const msg = `error infinite loop in class Program\n`
-                + `at pc = ${this.pc}\n`;
+                const msg = `error infinite loop, ran code twice at pc = ${this.pc-1}\n`;
                 return {msg, objectTrace};
             }
             pcs.set(this.pc, i);
             this.execute(code);
-            this.pc++;
             this.trace.push(this.getNowTrace());
         }
         return 0;
@@ -49,6 +46,7 @@ class Program {
     execute(line){
         const opp = this.getOpp(line.opp);
         opp(this, line.val);
+        this.pc++;
     }
     getOpp(opcode){
         return Program.ops[opcode];
@@ -59,23 +57,23 @@ class Program {
         nop: (program) => {}
     }
     static objectTrace(lines, trace){
-        if ( trace.length < 1)return "no trace";
+        if ( trace.length < 1)return [];
         const {pc, acc} = trace[0];
         const objectTrace = [{pc, acc}]
         for (let i = 1; i < trace.length; i++){
-            const {pc:line} = trace[i-1];
-            const codeO = lines[line];
-            const {opp, val} = codeO;
+            const {pc:lineNr} = trace[i-1];
+            const line = lines[lineNr];
+            const {opp, val, comment} = line;
             const code = `${opp} ${val}`;
             const {pc, acc} = trace[i];
-            const obj = Object.freeze({line, code, pc, acc});
+            const obj = Object.freeze({lineNr: lineNr, code, pc, acc, comment});
             objectTrace.push(obj);
         }
         return objectTrace;
     }
     static stringTrace(lines, trace){
         const objectTrace = Program.objectTrace(lines, trace);
-        return objectArrayToTableString(objectTrace, ["line", "code", "pc", "acc"]);
+        return objectArrayToTableString(objectTrace, ["lineNr", "code", "pc", "acc", "comment"]);
     }
 }
 function part1(input){
@@ -83,7 +81,7 @@ function part1(input){
     const program = new Program();
     const error =  program.run(input);
     if (error){
-        HTMLStringTable("error trace", error.objectTrace, ["line", "code", "pc", "acc"]);
+        HTMLStringTable(error.msg, error.objectTrace, ["lineNr", "code", "pc", "acc", "comment"]);
         return `error | pc: ${program.pc}, acc: ${program.acc}`;
     }
     return `success | pc: ${program.pc}, acc: ${program.acc}`;
@@ -122,7 +120,7 @@ function part2(input){
             HTMLStringTable(
                 "program trace",
                 Program.objectTrace(edited, program.trace),
-                ["line", "code", "pc", "acc"]
+                ["lineNr", "code", "pc", "acc", "comment"]
             );
 
             return `success | line changed: ${index} | pc: ${program.pc}, acc: ${program.acc}`;
