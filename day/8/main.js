71435abbb02620c9c881c784a1ff7a262aa9671b
Bram Otte <59796107+BramOtte@users.noreply.github.com>
2020-12-08 12:18:56 +0100

    did part8 part 1

--- day/8/main.js
+++ day/8/main.js
@@ -1,14 +1,95 @@
-setDayNumber(-8);
+setDayNumber(8);
 testDay("--- Day 8 ? ---", getInput, part1, part2);
 
 async function getInput(href){
-    return (await fetch(href).then(res=>res.text()))
-        .split("\n").slice(0, -1)
-    ;
+    return Object.freeze(
+        (await fetch(href).then(res=>res.text()))
+        .split(/\r*\n/)
+        .filter( (line) => (/^\w{3} [+-]\d+/).test(line) )
+        .map( (line) => {
+            const [opp, val] = line.split(" ");
+            return Object.freeze({opp:opp, val:parseInt(val)});
+        })
+    );
+}
+class Program {
+    pc = 0;
+    acc = 0;
+    trace = [];
+    static maxTime = 1_000_000;
+    reset(){
+        this.pc = 0;
+        this.acc= 0;
+        this.trace = [this.getNowTrace()]
+    }
+    run(lines){
+        this.reset();
+        let pcs = new Map();
+        for (let i = 0; this.pc < lines.length; i++){
+            const code = lines[this.pc];
+            if (pcs.has(this.pc)){
+                let lastI = pcs.get(this.pc);
+                this.trace.push(this.getNowTrace());
+                const cTrace = this.trace.slice(lastI);
+                const stringTrace = Program.stringTrace(lines, cTrace);
+                const msg = `error infinite loop in class Program\n`
+                + `at pc = ${this.pc}\n`
+                + stringTrace;
+                throw new Error(msg);
+            }
+            pcs.set(this.pc, i);
+            this.execute(code);
+            this.pc++;
+            this.trace.push(this.getNowTrace());
+        }
+        return 0;
+    }
+    getNowTrace(){
+        return Object.freeze({pc: this.pc, acc: this.acc});
+    }
+    execute(line){
+        const opp = this.getOpp(line.opp);
+        opp(this, line.val);
+    }
+    getOpp(opcode){
+        return Program.ops[opcode];
+    }
+    static ops = {
+        acc: (program, val) => program.acc += val,
+        jmp: (program, val) => program.pc += val-1,
+        nop: (program) => {}
+    }
+    static objectTrace(lines, trace){
+        if ( trace.length < 1)return "no trace";
+        const {pc, acc} = trace[0];
+        const objectTrace = [{pc, acc}]
+        for (let i = 1; i < trace.length; i++){
+            const {pc:line} = trace[i-1];
+            const codeO = lines[line];
+            console.log(line, codeO);
+            const {opp, val} = codeO;
+            const code = `${opp} ${val}`;
+            const {pc, acc} = trace[i];
+            const obj = Object.freeze({line, code, pc, acc});
+            objectTrace.push(obj);
+        }
+        return objectTrace;
+    }
+    static stringTrace(lines, trace){
+        const objectTrace = Program.objectTrace(lines, trace);
+        return objectArrayToTableString(objectTrace, ["line", "code", "pc", "acc"]);
+    }
 }
 function part1(input){
-    let result = 1;
-    return result;
+    const program = new Program();
+    try {
+        program.run(input);
+    } catch(e){
+        console.error(e.message);
+        return program.acc;
+        // return `ran with error | pc:${program.pc}, acc:${program.acc}`
+    }
+    return `success | pc:${program.pc}, acc:${program.acc}`;
 }
 
 function part2(input){
